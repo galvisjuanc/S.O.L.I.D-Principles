d1a3be8a6ea3f0207fd80103a82b4070fa8c51a8
Mauricio Coral <Coral>
2025-02-03 08:35:34 -0500

    Using interfaces to simulate the first principle

--- 2-OpenClose/EmployeeFullTime.cs
+++ 2-OpenClose/EmployeeFullTime.cs
@@ -1,18 +1,20 @@
-namespace OpenClose
-{
-    public class EmployeeFullTime : Employee
-    {
-        public EmployeeFullTime(string fullname, int hoursWorked)
-        {
-            Fullname = fullname;
-            HoursWorked = hoursWorked;
-        }
+using OpenClose;
 
-        public override decimal CalculateSalaryMonthly()
-        {
-            decimal hourValue = 30000M;
-            decimal salary = hourValue * HoursWorked;
-            return salary;
-        }
+public class EmployeeFullTime : IEmployee
+{
+    public string Fullname { get; set; }
+    public int HoursWorked { get; set; }
+
+    public EmployeeFullTime(string fullname, int hoursWorked)
+    {
+        Fullname = fullname;
+        HoursWorked = hoursWorked;
+    }
+
+    public decimal CalculateSalaryMonthly()
+    {
+        decimal hourValue = 30000M;
+        decimal salary = hourValue * HoursWorked;
+        return salary;
     }
 }
