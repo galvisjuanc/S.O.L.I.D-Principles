b9b2c04a2da9274d98e00a7e0460acf6cbe2f6e2
Mauricio Coral <Coral>
2025-02-03 08:05:45 -0500

    New EmployeeContractor class extended from Employee

---
+++ 2-OpenClose/EmployeeContractor.cs
@@ -0,0 +1,18 @@
+namespace OpenClose
+{
+    public class EmployeeContractor : Employee
+    {
+        public EmployeeContractor(string fullname, int hoursWorked)
+        {
+            Fullname = fullname;
+            HoursWorked = hoursWorked;
+        }
+
+        public override decimal CalculateSalaryMonthly()
+        {
+            decimal hourValue = 18000M;
+            decimal salary = hourValue * HoursWorked;
+            return salary;
+        }
+    }
+}
