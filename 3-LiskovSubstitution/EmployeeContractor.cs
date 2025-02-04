c6f9683580e3b111c6fd296c3d463ad42e3bb665
Mauricio Coral <Coral>
2025-02-04 08:31:39 -0500

    Using Liskov Principle II

--- 3-LiskovSubstitution/EmployeeContractor.cs
+++ 3-LiskovSubstitution/EmployeeContractor.cs
@@ -5,5 +5,11 @@ namespace Liskov
         public EmployeeContractor(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
         {
         }
+
+        public override decimal CalculateSalary()
+        {
+            decimal hourValue = 40;
+            return hourValue * (HoursWorked);
+        }
     }
 }
